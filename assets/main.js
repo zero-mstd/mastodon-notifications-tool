8e2e1054c5fb6061a43e4e108d943432f4d83015
zero-mstd <zer0zero-mstd@yahoo.com>
2022-01-08 21:39:52 +0800

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,87 @@
+'use strict'
+
+if (!('content' in document.createElement('template'))) {
+    alert('Your browser doesn’t support the HTML template.')
+}
+
+document.getElementById('noti-file-input')
+    .addEventListener('change', function(event) {
+        var file = event.target.files[0];
+        noti_input(file);
+    });
+
+function noti_input(file) {
+    var reader = new FileReader();
+    reader.addEventListener('load', function() {
+        var noti = JSON.parse(this.result);
+        deal_with_noti(noti);
+    });
+    reader.readAsText(file);
+}
+
+var notisTable;
+
+function deal_with_noti(noti) {
+    let notifications_list = noti.notifications_list;
+    let url_content_pair = noti.url_content_pair;
+
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template
+    var tbody = document.querySelector('tbody');
+    var template = document.querySelector('#tr-temp');
+
+    notifications_list.forEach(function(item, index) {
+        var clone = template.content.cloneNode(true);
+        var td = clone.querySelectorAll('td');
+        td[0].textContent = item[0] + ' (' + (index + 1).toString() + ')';
+        td[1].textContent = item[1];
+        td[2].textContent = item[2];
+        td[3].textContent = 'u:' + item[3];
+        td[4].textContent = 's:' + item[4];
+
+        let s_url = item[4];
+        let s_content;
+        if (s_url == '-') {
+            s_content = 'Type follow or follow_request doesn’t have a status.';
+        } else {
+            s_content = url_content_pair[s_url];
+        }
+        td[5].textContent = s_content;
+
+        tbody.appendChild(clone);
+    })
+
+    var options = {
+        valueNames: ['i', 't', 'a', 'u', 's', 'c'],
+        listClass: 'notifications',
+        searchClass: 'search-status',
+        searchColumns: ['u', 's', 'c'],
+        searchDelay: 500,
+    };
+
+    notisTable = new List('notis', options);
+}
+
+// https://stackoverflow.com/questions/14544104/
+var checkboxes = document.querySelectorAll('input[type=checkbox][name=c-type]');
+checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+        let selected_tpye =
+            Array.from(checkboxes)
+            .filter(i => i.checked)
+            .map(i => i.value);
+        notisTable.filter(function(item) {
+            if (selected_tpye.includes(item.values().t)) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+    })
+});
+
+function show(e) {
+    // https://developer.mozilla.org/zh-CN/docs/Web/API/Element/previousElementSibling
+    let content = e.previousElementSibling.textContent;
+    // innerHTML to escape; textContent parse them.
+    document.getElementById('content').innerHTML = content;
+}
---
+++ export.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+import sys
+import json
+import requests
+import re
+
+############### You should change the following two lines first ###############
+headers = {'Authorization': 'Bearer <your_access_token_here>'}
+# (e.g.) headers = {'Authorization': 'Bearer AbcDEFg1HIJk2L3M4NopqRST5uvwx6yZAbCde7f8gHi'}
+domain = '<your_domain_here>'
+# (e.g.) domain = 'https://mastodon.social'
+###############################################################################
+
+save_frequence = 30
+file_name = 'notifications.json'
+url = domain + '/api/v1/notifications'
+argv = ''
+since_id = 0
+new_list = []
+if len(sys.argv) == 2:
+    if sys.argv[1] == '--continue':
+        argv = 'c'
+    elif sys.argv[1] == '--update':
+        argv = 'u'
+
+def deal_with_data(data, i, t, a, u, s, c):
+    if s in data['url_content_pair']:
+        pass
+    else:
+        data['url_content_pair'][s] = c
+    new_list.append([i, t, a, u, s])
+
+def read_file():
+    if argv != '':
+        with open(file_name, 'r') as rf:
+            data = json.load(rf)
+            if argv == 'c':
+                max_id = data['notifications_list'][-1][0]
+                global url
+                url = domain + '/api/v1/notifications?max_id=' + str(max_id)
+            elif argv == 'u':
+                global since_id
+                since_id = data['notifications_list'][0][0]
+    else:
+        data = {
+                'notifications_list':[],
+                'url_content_pair':{}
+                }
+    return data
+
+def write_file(data):
+    with open(file_name, 'w') as rf:
+        if argv == 'u':
+            data['notifications_list'][0:0] = new_list
+        else:
+            data['notifications_list'].extend(new_list)
+        json.dump(data, rf, ensure_ascii=False, indent=2)
+    new_list.clear()
+    print('    Write in.')
+
+data = read_file()
+p = 1    # page count
+while 1:
+    nr = requests.get(url, headers = headers)
+    if nr.status_code == 429:
+        reset = nr.headers['x-ratelimit-reset']
+        print('You run out the rate limits, which will be reset at ' + reset)
+        print('After that, you can run: python export.py --continue')
+        break
+    nj = nr.json()
+    q = 0    # item count in this page
+    for e in nj:
+        i = e['id']
+        if i == since_id:
+            p = 0    # used for break outsider
+            break
+        t = e['type']
+        a = e['created_at']
+        u = e['account']['acct']
+        if t[:6] == 'follow':    # type follow or follow_request has no status
+            s = '-'
+            c = '-'
+        else:
+            s = e['status']['url']
+            c = e['status']['content']
+        deal_with_data(data, i, t, a, u, s, c)
+        q = q + 1
+    print('Page ' + str(p) + ', ' + str(q) + ' notifications.', end=" ")
+    if ('Link' in nr.headers) and ('rel="next"' in nr.headers['Link']):
+        if p == 0:
+            print('Updated.')
+            break
+        next_url = re.match(r"\<(.*?)\>", nr.headers['Link']).groups()[0]
+        url = next_url
+        print('Next url: ' + url)
+        if p % save_frequence == 0:
+            write_file(data)
+        p = p + 1
+    else:
+        print('Done.')
+        break
+
+write_file(data)
+
